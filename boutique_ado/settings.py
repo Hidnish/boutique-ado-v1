447e41e70ebaca90a995a91511d5d8459e222207
Hidnish <hideonishimura@live.it>
2021-10-18 13:19:08 +0000

    Add homepage content

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
+
 ]
 
 MIDDLEWARE = [
@@ -60,7 +62,11 @@ ROOT_URLCONF = 'boutique_ado.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            # HIDEO add this
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
