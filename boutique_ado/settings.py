b8dddd7162e803579ada3a003e330ca672d65225
Hidnish <hideonishimura@live.it>
2021-10-19 12:39:08 +0000

    Added mobile header and navbar

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -154,7 +154,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # HIDEO add this
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
