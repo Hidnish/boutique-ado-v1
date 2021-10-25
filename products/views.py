4446d84a7d3b05177a0cdbb984542015cfd69fdc
Hidnish <hideonishimura@live.it>
2021-10-25 13:43:27 +0000

    add sorting JS and back to top list

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 # way to handle queries (check documentation)
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category
 
 # Create your views here.
@@ -19,27 +20,29 @@ def all_products(request):
     if request.GET:
 
         if 'sort' in request.GET:
-            sortkey = request.GET['sort'] # Create the sortkey variable
-            sort = sortkey # Make a copy of it and call it sort, which we'll use later to construct current_sorting = f'{sort}_{direction}'
-            if sortkey == 'name': # If the field we want to sort on is 'name'...
-                sortkey = 'lower_name' # Let's actually sort (i.e. order_by) on one called 'lower_name', in order to ensure it doesn't order Z before a just because the Z is uppercase
-                products = products.annotate(lower_name=Lower('name')) # Annotate all the products w/ a new field, lower_name=Lower('name') and sort based on it
+            # Create the sortkey variable
+            sortkey = request.GET['sort']
+            # Make a copy of it and call it sort which we'll use later to construct current_sorting =                     f'{sort}_{direction}'
+            sort = sortkey
+            # If the field we want to sort on is 'name'
+            if sortkey == 'name':
+                # Let's actually sort (i.e. order_by) on one called 'lower_name', in order to ensure it doesn't order Z before a just because the Z is uppercase
+                sortkey = 'lower_name'
+                # Annotate all the products w/ a new field, lower_name=Lower('name') and sort based on it
+                products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
                 sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
-            products = products.order_by(sortkey) # --> this becomes products.order_by('lower_name') now
+            products = products.order_by(sortkey)  # --> this becomes products.order_by('lower_name') now
 
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
-            # double underscore syntax (__name__) is common when making queries in django
-            # here you are looking for the name field of the category model
-            # cause category and product are related with FK
+            # double underscore syntax (__name__) is common when making queries in django here you are looking for the name field of the category model cause category and product are related with FK
             products = products.filter(category__name__in=categories)
-            # convert string in list of categories into category objects to
-            # be accessed in the template
+            # convert string in list of categories into category objects to be accessed in the template
             categories = Category.objects.filter(name__in=categories)
 
         if 'q' in request.GET:
