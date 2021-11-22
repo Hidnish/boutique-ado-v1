21d99c4dbf1ed6b6da3cec8af103a028e35a5dd3
Hidnish <hideonishimura@live.it>
2021-11-22 01:23:33 +0000

    add profile form, views and updated template

--- profiles/models.py
+++ profiles/models.py
@@ -8,18 +8,18 @@ from django_countries.fields import CountryField
 
 class UserProfile(models.Model):
     """
-    A User profile model for maintaining default
-    delivery information and user history
-
+    A user profile model for maintaining default
+    delivery information and order history
     """
     # this acts as a foreign key for a one-to-one relation: user-profile
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    default_country = CountryField(blank_label='Country *', null=True, blank=True)
-    default_postcode = models.CharField(max_length=20, null=True, blank=True)
-    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
+    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
     default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     default_county = models.CharField(max_length=80, null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_country = CountryField(blank_label='Country', null=True, blank=True)
 
     def __str__(self):
         return self.user.username
@@ -28,9 +28,9 @@ class UserProfile(models.Model):
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
-    Create or Update user profile
+    Create or update the user profile
     """
     if created:
         UserProfile.objects.create(user=instance)
-    # existing user save profile
+    # Existing users: just save the profile
     instance.userprofile.save()
